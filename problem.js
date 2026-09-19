8ac4cc2cb593b44ebb26aa48091eed92b62010ad
app285 <uzamerika261@gmail.com>
2026-09-19 08:18:37 +0500

    feat: add algotirhm solutions

---
+++ problem.js
@@ -0,0 +1,14 @@
+function countVowels(str) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log("Vowel count:", countVowels("JAVASCRIPT"));
